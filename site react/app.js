71422afa873871433321418ef29ead278a945d0e
João Vitor Rodrigues silva Moreira <159218992+jovigod888@users.noreply.github.com>
2024-05-29 03:12:51 -0300

    Add files via upload

---
+++ site react/app.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import HelloWorld from './helloworld';
+
+const App = () => {
+    return (
+        <div>
+            <HelloWorld />
+        </div>
+    );
+};
+
+export default App;
